9f322ae789f763049f83af0c1e1e42eb31ff6d74
Barcuh <35744245+Barcuh@users.noreply.github.com>
2021-04-12 13:34:27 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function setup() {
     cnv.parent("p5canvas");
 
     vidcapture = createCapture(VIDEO);
-    vidcapture.size(width, height);
+    vidcapture.size(600, 600);
     vidcapture.hide();
     ctracker = new clm.tracker();
     ctracker.init();
